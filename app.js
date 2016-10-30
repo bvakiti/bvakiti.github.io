2a8cb3b52167260a70e53d267f0e0f851afbf200
bvakiti <bharathv1590@gmail.com>
2016-10-30 22:34:51 +0530

    adding manifest file

--- app.js
+++ app.js
@@ -32,7 +32,58 @@ myApp.service('FlightDetailsService', function ($http, $q) {
 
 if('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/serviceworker.js')
-        .then(function() { console.log("Service Worker Registered"); }).catch(function (err) {
+        .then(function() {
+            console.log("Service Worker Registered");
+            initialiseState();
+        }).catch(function (err) {
+            window.Demo.debug.log('Service workers aren\'t supported in this browser.');
             console.log('err  ' + err);
         });
+}
+
+function initialiseState() {
+    // Are Notifications supported in the service worker?
+    if (!('showNotification' in ServiceWorkerRegistration.prototype)) {
+        window.Demo.debug.log('Notifications aren\'t supported.');
+        return;
+    }
+
+    if (Notification.permission !== 'granted') {
+        console.log('The user has not granted the notification permission.');
+        return;
+    } else if (Notification.permission === 'blocked') {
+        /* the user has previously denied push. Can't reprompt. */
+    } else {
+        /* show a prompt to the user */
+    }
+
+    // Check if push messaging is supported
+    if (!('PushManager' in window)) {
+        window.Demo.debug.log('Push messaging isn\'t supported.');
+        return;
+    }
+
+    // Use serviceWorker.ready so this is only invoked
+    // when the service worker is available.
+    navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
+        return serviceWorkerRegistration.pushManager.subscribe({
+            'userVisibleOnly': true
+    });
+        //serviceWorkerRegistration.pushManager.getSubscription()
+
+    }).then(function(subscription) {
+        if (!subscription) {
+            // Set appropriate app states.
+            return;
+        }
+        console.log(subscription.endpoint);
+        //var fetchOptions = {
+        //    method: 'post',
+        //    headers: new Headers({
+        //        'Content-Type': 'application/json'
+        //    }),
+        //    body: JSON.stringify(subscription)
+        //};
+        //return fetch('/your-web-server/api', fetchOptions);
+    });
 }
